59a012bd03226927117f97f00e9fff9e57435c1b
Evgeny_Ch <gek_sh@mail.ru>
2023-02-17 23:13:52 +0300

    add menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,19 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        String user = Cli.greeting();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        int gameNumber = sc.nextInt();
+        System.out.println("Your choice: " + gameNumber);
+        System.out.println(" ");
+        if (gameNumber == 1) {
+            System.out.println("Welcome to the Brain Games!");
+            String user = Cli.greeting();
+        }
     }
 }
