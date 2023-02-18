38c5687f9d4ccecb78b0e8d054e0c3e0df12bbe7
Evgeny_Ch <gek_sh@mail.ru>
2023-02-18 23:17:06 +0300

    call even

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,16 +4,30 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        int gameNumber = 0;
+        String userName = "";
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
+        System.out.println("2 - Even");
         System.out.println("0 - Exit");
-        int gameNumber = sc.nextInt();
+        if (sc.hasNextInt()) {
+            gameNumber = sc.nextInt();
+        }
+        sc.nextLine();
         System.out.println("Your choice: " + gameNumber);
         System.out.println(" ");
         if (gameNumber == 1) {
             System.out.println("Welcome to the Brain Games!");
-            String user = Cli.greeting();
+            userName = Cli.greeting();
+        }
+        if (gameNumber == 2) {
+            Cli.greeting();
+            if (Even.evenGame()) {
+                System.out.println("Congratulations, " + userName);
+            } else {
+                System.out.println("Lets try again, " + userName);
+            }
         }
     }
 }
