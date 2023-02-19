99bdb37f38cd5c4e7fda8dcb2e5d44903a4ddd07
Evgeny_Ch <gek_sh@mail.ru>
2023-02-19 15:01:56 +0300

    Update user input

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,14 +1,10 @@
 package hexlet.code;
-import java.util.Scanner;
 
 public class Cli {
     public static String greeting() {
-        Scanner sc = new Scanner(System.in);
         System.out.print("May i have your name? ");
-        String userName = sc.next();
+        String userName = App.userInput();
         System.out.println("Hello, " + userName + "!");
-        sc.hasNextLine();
-        sc.close();
         return userName;
     }
 }
