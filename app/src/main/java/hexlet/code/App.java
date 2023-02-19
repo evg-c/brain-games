79e918d8b2dec6c124a0dd205bcd9df305f6187d
Evgeny_Ch <gek_sh@mail.ru>
2023-02-19 15:01:43 +0300

    Update user input

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,30 +4,28 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        int gameNumber = 0;
-        String userName = "";
-        Scanner sc = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("0 - Exit");
-        if (sc.hasNextInt()) {
-            gameNumber = sc.nextInt();
-        }
-        sc.nextLine();
+        String gameNumber = userInput();
         System.out.println("Your choice: " + gameNumber);
         System.out.println(" ");
-        if (gameNumber == 1) {
+        if (gameNumber.equals("1")) {
             System.out.println("Welcome to the Brain Games!");
-            userName = Cli.greeting();
-        }
-        if (gameNumber == 2) {
             Cli.greeting();
+        }
+        if (gameNumber.equals("2")) {
+            String userName = Cli.greeting();
             if (Even.evenGame()) {
                 System.out.println("Congratulations, " + userName);
             } else {
                 System.out.println("Lets try again, " + userName);
             }
         }
+    }
+    public static String userInput() {
+        Scanner sc = new Scanner(System.in);
+        return sc.next();
     }
 }
