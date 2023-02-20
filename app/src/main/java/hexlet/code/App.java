177849d343ccd5f9e33a402a56ca2fa7dd2927b5
Evgeny_Ch <gek_sh@mail.ru>
2023-02-20 23:08:36 +0300

    add Calc game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,17 +7,25 @@ public class App {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
         String gameNumber = userInput();
         System.out.println("Your choice: " + gameNumber);
         System.out.println(" ");
         if (gameNumber.equals("1")) {
-            System.out.println("Welcome to the Brain Games!");
             Cli.greeting();
         }
         if (gameNumber.equals("2")) {
             String userName = Cli.greeting();
             if (Even.evenGame()) {
+                System.out.println("Congratulations, " + userName);
+            } else {
+                System.out.println("Lets try again, " + userName);
+            }
+        }
+        if (gameNumber.equals("3")) {
+            String userName = Cli.greeting();
+            if (Calc.calcGame()) {
                 System.out.println("Congratulations, " + userName);
             } else {
                 System.out.println("Lets try again, " + userName);
