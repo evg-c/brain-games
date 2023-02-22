0820c56eba56e70d08c7dbd5970b1267fd8ecf24
Evgeny_Ch <gek_sh@mail.ru>
2023-02-23 00:16:15 +0300

    Add Engine

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class App {
+    //public static String gameNumber;
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
@@ -12,25 +13,33 @@ public class App {
         String gameNumber = userInput();
         System.out.println("Your choice: " + gameNumber);
         System.out.println(" ");
-        if (gameNumber.equals("1")) {
-            Cli.greeting();
-        }
-        if (gameNumber.equals("2")) {
-            String userName = Cli.greeting();
-            if (Even.evenGame()) {
-                System.out.println("Congratulations, " + userName);
-            } else {
-                System.out.println("Lets try again, " + userName);
-            }
-        }
-        if (gameNumber.equals("3")) {
-            String userName = Cli.greeting();
-            if (Calc.calcGame()) {
-                System.out.println("Congratulations, " + userName);
-            } else {
-                System.out.println("Lets try again, " + userName);
-            }
-        }
+        Engine.general(gameNumber);
+        //switch (gameNumber) {
+        //    case ("1") -> Cli.greeting();
+        //    case ("2") -> Even.evenGame();
+        //    case ("3") -> Calc.calcGame();
+        //    default -> { }
+        //}
+
+        //if (gameNumber.equals("1")) {
+        //    Cli.greeting();
+        //}
+        //if (gameNumber.equals("2")) {
+        //    String userName = Cli.greeting();
+        //    if (Even.evenGame()) {
+        //        System.out.println("Congratulations, " + userName);
+        //    } else {
+        //        System.out.println("Lets try again, " + userName);
+        //    }
+        //}
+        //if (gameNumber.equals("3")) {
+        //    String userName = Cli.greeting();
+        //    if (Calc.calcGame()) {
+        //        System.out.println("Congratulations, " + userName);
+        //    } else {
+        //        System.out.println("Lets try again, " + userName);
+        //    }
+        //}
     }
     public static String userInput() {
         Scanner sc = new Scanner(System.in);
