a7cf139ddd56f3bc7dd851b728ae7330508d9a87
Evgeny_Ch <gek_sh@mail.ru>
2023-02-24 21:54:03 +0300

    Add GCD.java in games

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,15 +1,13 @@
 package hexlet.code;
 import hexlet.code.games.Even;
 import hexlet.code.games.Calc;
+import hexlet.code.games.GCD;
 
 public class Engine {
-    //public static String question;
     public static final int ROUND = 100; // для получения целого случайного числа из double в диапазоне до 100
     public static final int ROUND2 = 3;  // для получения целого случайного числа из double в диапазоне до 3
     public static final int COUNT = 3;   // число попыток
-    //public static String rightAnswer;
     public static void general(String gameNumber) {
-        //Cli.greeting();
         String userName = "";
         if (gameNumber.equals("0")) {
             return;
@@ -24,6 +22,7 @@ public class Engine {
         switch (gameNumber) {
             case ("2") -> Even.announce();
             case ("3") -> Calc.announce();
+            case ("4") -> GCD.announce();
             default -> { }
         }
         String[] pairQuestionAnswer = new String[2];
@@ -31,17 +30,15 @@ public class Engine {
             switch (gameNumber) {
                 case ("2") -> Even.task(pairQuestionAnswer);
                 case ("3") -> Calc.task(pairQuestionAnswer);
+                case ("4") -> GCD.task(pairQuestionAnswer);
                 default -> { }
             }
-            //System.out.println("Question: " + Engine.question);
             System.out.println("Question: " + pairQuestionAnswer[0]);
             System.out.print("Your answer: ");
             String userAnswer = App.userInput();
-            //if (userAnswer.trim().equals(Engine.rightAnswer)) {
             if (userAnswer.trim().equals(pairQuestionAnswer[1])) {
                 System.out.println("Correct!");
             } else {
-                //String longOut = userAnswer + " is wrong answer ;(. Correct answer was " + Engine.rightAnswer + ".";
                 String longOut = userAnswer + " is wrong answer ;(. Correct answer was " + pairQuestionAnswer[1] + ".";
                 System.out.println(longOut);
                 result = false;
