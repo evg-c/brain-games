bcda915e171f8901a87c3fd9c4b19806b4c81397
Evgeny_Ch <gek_sh@mail.ru>
2023-02-26 14:09:16 +0300

    Replacing i with I

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,7 @@ package hexlet.code;
 public class Cli {
     public static String greeting() {
         System.out.println("Welcome to the Brain Games!");
-        System.out.print("May i have your name? ");
+        System.out.print("May I have your name? ");
         String userName = App.userInput();
         System.out.println("Hello, " + userName + "!");
         return userName;
