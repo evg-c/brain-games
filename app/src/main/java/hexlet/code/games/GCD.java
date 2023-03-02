ed8fe40441e61c1da5274eeeea02cef297d90212
Evgeny_Ch <gek_sh@mail.ru>
2023-03-02 23:06:10 +0300

    Refactoring

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -8,22 +8,23 @@ public class GCD {
     }
 
     public static String[] task(String[] pairQuestionAnswer) {
-        int a1;
-        int a2;
-        a1 = (int) (Math.random() * Engine.ROUND);
-        a2 = (int) (Math.random() * Engine.ROUND);
+        int a1 = (int) (Math.random() * Engine.ROUND);  // генерация первого случайного числа
+        int a2 = (int) (Math.random() * Engine.ROUND);  // генерация второго случайного числа
         pairQuestionAnswer[0] = a1 + " " + a2;
-        if (Math.abs(a1) == 0 || Math.abs(a2) == 0) {
-            pairQuestionAnswer[1] = "0";
-            return pairQuestionAnswer;
-        }
-        if (Math.abs(a1) == 1 || Math.abs(a2) == 1) {
-            pairQuestionAnswer[1] = "1";
-            return pairQuestionAnswer;
-        }
         if (Math.abs(a1) == Math.abs(a2)) {
             pairQuestionAnswer[1] = Integer.toString(a1);
             return pairQuestionAnswer;
+        }
+        pairQuestionAnswer[1] = Integer.toString(nod(a1, a2));
+        return pairQuestionAnswer;
+    }
+
+    public static int nod(int a1, int a2) {
+        if (anyNumberIs(0, a1, a2)) {
+            return 0;
+        }
+        if (anyNumberIs(1, a1, a2)) {
+            return 1;
         }
         int nod = 0;
         while ((Math.abs(a1) > 0) && (Math.abs(a2) > 0)) {
@@ -40,7 +41,12 @@ public class GCD {
                 }
             }
         }
-        pairQuestionAnswer[1] = Integer.toString(nod);
-        return pairQuestionAnswer;
+        return nod;
+    }
+    public static boolean anyNumberIs(int number, int a1, int a2) {
+        if (Math.abs(a1) == number || Math.abs(a2) == number) {
+            return true;
+        }
+        return false;
     }
 }
