f74a1e69dc1dc80a4551b80cf0f59980add1c237
Evgeny_Ch <gek_sh@mail.ru>
2023-03-08 01:04:45 +0300

    Update structure

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,42 +1,67 @@
 package hexlet.code.games;
 import hexlet.code.Engine;
+import java.util.Random;
 
 public class Calc {
-    public static void announce() {
-        System.out.println("What is the result of the expression?");
-    }
-
-    public static String[] task(String[] pairQuestionAnswer) {
-        int a1;
-        int a2;
-        int a3;
-        int rez;
-        final int prob = 4; // выдача выражения 2 * 2 и сравнение с 4 для непредвиденного случая
-        a1 = (int) (Math.random() * Engine.ROUND);
-        a2 = (int) (Math.random() * Engine.ROUND);
-        a3 = (int) (Math.random() * Engine.ROUND2); // генерация случайного числа операции
-        // 0 - это +
-        // 1 - это -
-        // 2 - это *
-        switch (a3) {
-            case 0 -> {
-                pairQuestionAnswer[0] = a1 + " + " + a2;
-                rez = a1 + a2;
-            }
-            case 1 -> {
-                pairQuestionAnswer[0] = a1 + " - " + a2;
-                rez = a1 - a2;
-            }
-            case 2 -> {
-                pairQuestionAnswer[0] = a1 + " * " + a2;
-                rez = a1 * a2;
-            }
-            default -> {
-                pairQuestionAnswer[0] = "2 + 2";
-                rez = prob;
+    public static void task() {
+        String ruleOfGame = "What is the result of the expression?";  // правило игры
+        String username = Engine.startAndReceiveName(ruleOfGame);
+        Random rnd = new Random();
+        final int scopeOfRandomTo100 = 100;  // диапазон генерации случайного числа (до 100)
+        final int countOfTests = 3;          // число попыток
+        final int scopeOfRandomTo3 = 3;      // диапазон генерации случайного числа (до 3)
+        for (int i = 1; i <= countOfTests; i++) {
+            int randomNumber1 = rnd.nextInt(scopeOfRandomTo100); // переменная для хранения случайного числа 1
+            int randomNumber2 = rnd.nextInt(scopeOfRandomTo100); // переменная для хранения случайного числа 2
+            int randomNumber3 = rnd.nextInt(scopeOfRandomTo3);   // переменная для хранения случайного типа операции
+            // 0 - это +
+            // 1 - это -
+            // 2 - это *
+            String question1 = generateQuestion(randomNumber1, randomNumber2, randomNumber3);
+            String answer = Integer.toString(generateAnswer(randomNumber1, randomNumber2, randomNumber3));
+            if (!Engine.compareQuestionAnswer(question1, answer)) {
+                Engine.finish(username, false);
+                return;
             }
         }
-        pairQuestionAnswer[1] = Integer.toString(rez);
-        return pairQuestionAnswer;
+        Engine.finish(username, true);
+    }
+
+    public static String generateQuestion(int number1, int number2, int number3) {
+        String question;
+        switch (number3) {
+            case 0 -> {
+                question = number1 + " + " + number2;
+            }
+            case 1 -> {
+                question = number1 + " - " + number2;
+            }
+            case 2 -> {
+                question = number1 + " * " + number2;
+            }
+            default -> {
+                question = "2 + 2";
+            }
+        }
+        return question;
+    }
+    public static int generateAnswer(int number1, int number2, int number3) {
+        int resultOperation;
+        final int prob = 4;                  // выдача выражения 2 * 2 и сравнение с 4 для непредвиденного случая
+        switch (number3) {
+            case 0 -> {
+                resultOperation = number1 + number2;
+            }
+            case 1 -> {
+                resultOperation = number1 - number2;
+            }
+            case 2 -> {
+                resultOperation = number1 * number2;
+            }
+            default -> {
+                resultOperation = prob;
+            }
+        }
+        return resultOperation;
     }
 }
