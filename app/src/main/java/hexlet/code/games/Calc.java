deebb1a876a9564ff3cf23b341e5d6c4d8396f19
Evgeny_Ch <gek_sh@mail.ru>
2023-03-18 23:15:10 +0300

    Refactoring #2

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -3,31 +3,37 @@ import hexlet.code.Engine;
 import java.util.Random;
 
 public class Calc {
-    public static void task() {
-        String ruleOfGame = "What is the result of the expression?";  // правило игры
-        String username = Engine.startAndReceiveName(ruleOfGame);
+    static final String RULE_OF_GAME = "What is the result of the expression?";  // правило игры
+    static final int SCOPE_OF_RANDOM_TO_100 = 100;  // диапазон генерации случайного числа (до 100)
+    static final int COUNT_OF_TESTS = 3;          // число попыток
+    static final int SCOPE_OF_RANDOM_TO_3 = 3;      // диапазон генерации случайного числа (до 3)
+    public static void gameCalc() {
         Random rnd = new Random();
-        final int scopeOfRandomTo100 = 100;  // диапазон генерации случайного числа (до 100)
-        final int countOfTests = 3;          // число попыток
-        final int scopeOfRandomTo3 = 3;      // диапазон генерации случайного числа (до 3)
-        for (int i = 1; i <= countOfTests; i++) {
-            int randomNumber1 = rnd.nextInt(scopeOfRandomTo100); // переменная для хранения случайного числа 1
-            int randomNumber2 = rnd.nextInt(scopeOfRandomTo100); // переменная для хранения случайного числа 2
-            int randomNumber3 = rnd.nextInt(scopeOfRandomTo3);   // переменная для хранения случайного типа операции
-            // 0 - это +
-            // 1 - это -
-            // 2 - это *
-            String question1 = generateQuestion(randomNumber1, randomNumber2, randomNumber3);
-            String answer = Integer.toString(generateAnswer(randomNumber1, randomNumber2, randomNumber3));
-            if (!Engine.compareQuestionAnswer(question1, answer)) {
-                Engine.finish(username, false);
-                return;
+        String[][] arrayQuestionsAnswers = new String[COUNT_OF_TESTS][2];
+        String question;
+        String answer;
+        for (int i = 0; (i < arrayQuestionsAnswers.length); i++) {
+            int randomNumber1 = rnd.nextInt(SCOPE_OF_RANDOM_TO_100); // переменная для хранения случайного числа 1
+            int randomNumber2 = rnd.nextInt(SCOPE_OF_RANDOM_TO_100); // переменная для хранения случайного числа 2
+            int randomNumber3 = rnd.nextInt(SCOPE_OF_RANDOM_TO_3);   // переменная для хранения случайного типа операции
+            // 0 это +     1 это -      2 это *
+            try {
+                question = generateQuestion(randomNumber1, randomNumber2, randomNumber3);
+            } catch (NotGenerateOperationException ex) {
+                question = ex.getQuestion();
             }
+            try {
+                answer = Integer.toString(generateAnswer(randomNumber1, randomNumber2, randomNumber3));
+            } catch (NotGenerateOperationException ex) {
+                answer = ex.getAnswer();
+            }
+            arrayQuestionsAnswers[i][0] = question;
+            arrayQuestionsAnswers[i][1] = answer;
         }
-        Engine.finish(username, true);
+        Engine.compareQuestionAnswer(RULE_OF_GAME, arrayQuestionsAnswers);
     }
 
-    public static String generateQuestion(int number1, int number2, int number3) {
+    public static String generateQuestion(int number1, int number2, int number3) throws NotGenerateOperationException {
         String question;
         switch (number3) {
             case 0 -> {
@@ -40,14 +46,14 @@ public class Calc {
                 question = number1 + " * " + number2;
             }
             default -> {
-                question = "2 + 2";
+                throw new NotGenerateOperationException();
             }
         }
         return question;
     }
-    public static int generateAnswer(int number1, int number2, int number3) {
+
+    public static int generateAnswer(int number1, int number2, int number3) throws NotGenerateOperationException {
         int resultOperation;
-        final int prob = 4;                  // выдача выражения 2 * 2 и сравнение с 4 для непредвиденного случая
         switch (number3) {
             case 0 -> {
                 resultOperation = number1 + number2;
@@ -59,7 +65,7 @@ public class Calc {
                 resultOperation = number1 * number2;
             }
             default -> {
-                resultOperation = prob;
+                throw new NotGenerateOperationException();
             }
         }
         return resultOperation;
