38896b5c87d1c836147013c4f87500e89b12de13
Evgeny_Ch <gek_sh@mail.ru>
2023-03-18 23:15:50 +0300

    Refactoring #2

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -1,27 +1,24 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-
 import java.util.Random;
 
 public class Prime {
-    public static void task() {
-        String rule = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";  // правило игры
+    static final String RULE = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";  // правило игры
+    static final int SCOPE_OF_RANDOM_TO_100 = 100;  // диапазон генерации случайного числа (до 100)
+    static final int COUNT_OF_TESTS = 3;          // число попыток
+    public static void gamePrime() {
         int randomNumber;                    // переменная для хранения случайного числа
-        final int scopeOfRandomTo100 = 100;  // диапазон генерации случайного числа (до 100)
-        String username = Engine.startAndReceiveName(rule);
-        final int countOfTests = 3;          // число попыток
         Random rnd = new Random();
-        for (int i = 1; i <= countOfTests; i++) {
-            randomNumber = rnd.nextInt(scopeOfRandomTo100);
+        String[][] arrayQuestionsAnswers = new String[COUNT_OF_TESTS][2];
+        for (int i = 0; (i < arrayQuestionsAnswers.length); i++) {
+            randomNumber = rnd.nextInt(SCOPE_OF_RANDOM_TO_100);
             String answer = (isPrime(randomNumber) ? "yes" : "no");
             String question = Integer.toString(randomNumber);
-            if (!Engine.compareQuestionAnswer(question, answer)) {
-                Engine.finish(username, false);
-                return;
-            }
+            arrayQuestionsAnswers[i][0] = question;
+            arrayQuestionsAnswers[i][1] = answer;
         }
-        Engine.finish(username, true);
+        Engine.compareQuestionAnswer(RULE, arrayQuestionsAnswers);
     }
 
     public static boolean isPrime(int number) {
