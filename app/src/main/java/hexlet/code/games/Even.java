3ca1fb0139da4b164b936f44e052e04be474121a
Evgeny_Ch <gek_sh@mail.ru>
2023-03-18 23:15:21 +0300

    Refactoring #2

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -3,23 +3,21 @@ import hexlet.code.Engine;
 import java.util.Random;
 
 public class Even {
-    public static void task() {
-        String ruleOfGame = "Answer 'yes' if the number is even, otherwise answer 'no'.";  // правило игры
-        String userName = Engine.startAndReceiveName(ruleOfGame);
+    static final String RULE_OF_GAME = "Answer 'yes' if the number is even, otherwise answer 'no'.";  // правило игры
+    static final int SCOPE_OF_RANDOM_TO_100 = 100;  // диапазон генерации случайного числа (до 100)
+    static final int COUNT_OF_TESTS = 3;          // число попыток
+    public static void gameEven() {
         Random rnd = new Random();
         int randomNumber;                    // переменная для хранения случайного числа
-        final int scopeOfRandomTo100 = 100;  // диапазон генерации случайного числа (до 100)
-        final int countOfTests = 3;          // число попыток
-        for (int i = 1; i <= countOfTests; i++) {
-            randomNumber = rnd.nextInt(scopeOfRandomTo100);
+        String[][] arrayQuestionsAnswers = new String[COUNT_OF_TESTS][2];
+        for (int i = 0; (i < arrayQuestionsAnswers.length); i++) {
+            randomNumber = rnd.nextInt(SCOPE_OF_RANDOM_TO_100);
             String question = Integer.toString(randomNumber);
             String answer = (isEven(randomNumber) ? "yes" : "no");
-            if (!Engine.compareQuestionAnswer(question, answer)) {
-                Engine.finish(userName, false);
-                return;
-            }
+            arrayQuestionsAnswers[i][0] = question;
+            arrayQuestionsAnswers[i][1] = answer;
         }
-        Engine.finish(userName, true);
+        Engine.compareQuestionAnswer(RULE_OF_GAME, arrayQuestionsAnswers);
     }
 
     public static boolean isEven(int number) {
